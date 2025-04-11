bfd049f9a6ef2e8e97ecada5f0db887b1ae0bf4c
ksrikar1234 <49874498+ksrikar1234@users.noreply.github.com>
2025-04-11 12:41:39 +0530

    Update gp_array.hpp

--- include/containers/gp_array.hpp
+++ include/containers/gp_array.hpp
@@ -3,6 +3,14 @@
 #include <initializer_list>
 #include <algorithm>
 #include <stdexcept>
+#pragma once
+
+#include <cstddef>
+#include <new>
+#include <utility>
+#include <type_traits>
+#include <algorithm>
+#include <stdexcept>
 
 namespace gp_std
 {
@@ -10,151 +18,142 @@ namespace gp_std
     class array
     {
     private:
-        T m_data[N]; // Fixed-size internal storage
-
+        typename std::aligned_storage<sizeof(T), alignof(T)>::type m_data[N];
+    
+        T* ptr(std::size_t index) noexcept { return reinterpret_cast<T*>(&m_data[index]); }
+        const T* ptr(std::size_t index) const noexcept { return reinterpret_cast<const T*>(&m_data[index]); }
+    
     public:
-        // Type definitions
-        typedef T value_type;
-        typedef std::size_t size_type;
-        typedef T& reference;
-        typedef const T &const_reference;
-        typedef T* pointer;
-        typedef const T* const_pointer;
-        typedef T* iterator;
-        typedef const T* const_iterator;
-
-        // Default constructor (value-initializes elements)
-        array() : m_data{} {}
-
-        // Initializer list constructor (C++11)
+        using value_type = T;
+        using size_type = std::size_t;
+        using reference = T&;
+        using const_reference = const T&;
+        using pointer = T*;
+        using const_pointer = const T*;
+        using iterator = T*;
+        using const_iterator = const T*;
+    
+        // Default Constructor (only if T is default constructible)
+        array() requires std::is_default_constructible<T>::value
+        {
+            for (size_type i = 0; i < N; ++i)
+                new (&m_data[i]) T();
+        }
+    
+        // Variadic Constructor: Perfect Forward to all elements
+        template <typename... Args>
+        array(Args&&... args)
+        {
+            for (size_type i = 0; i < N; ++i)
+                new (&m_data[i]) T(std::forward<Args>(args)...);
+        }
+    
+        // Initializer list constructor
         array(std::initializer_list<T> init)
         {
-            std::size_t i = 0;
-            for (auto it = init.begin(); it != init.end() && i < N; ++it, ++i)
-            {
-                m_data[i] = *it;
-            }
+            size_type i = 0;
+            for (auto& elem : init)
+                new (&m_data[i++]) T(elem);
+    
             for (; i < N; ++i)
-            { // Zero-initialize remaining elements
-                m_data[i] = T();
+                new (&m_data[i]) T();
+        }
+    
+        // Copy Constructor
+        array(const array& other)
+        {
+            for (size_type i = 0; i < N; ++i)
+                new (&m_data[i]) T(other[i]);
+        }
+    
+        // Move Constructor
+        array(array&& other) noexcept
+        {
+            for (size_type i = 0; i < N; ++i)
+                new (&m_data[i]) T(std::move(other[i]));
+        }
+    
+        ~array()
+        {
+            for (size_type i = 0; i < N; ++i)
+                ptr(i)->~T();
+        }
+    
+        array& operator=(const array& other)
+        {
+            if (this != &other)
+            {
+                for (size_type i = 0; i < N; ++i)
+                    (*ptr(i)) = other[i];
             }
+            return *this;
         }
-
-        array(const array<T, N>& other)
+    
+        array& operator=(array&& other) noexcept
         {
-            for (size_t i = 0; i < N; ++i)
-            { (*this)[i] = other[i]; }
+            if (this != &other)
+            {
+                for (size_type i = 0; i < N; ++i)
+                    (*ptr(i)) = std::move(other[i]);
+            }
+            return *this;
         }
-
-        array(array<T, N>&& other)
-        {
-            for (size_t i = 0; i < N; ++i)
-            { (*this)[i] = std::move(other[i]);}
-        }
-
-        // Element access
-        reference operator[](size_type index) { return m_data[index]; }
-        const_reference operator[](size_type index) const { return m_data[index]; }
-
+    
+        reference operator[](size_type index) { return *ptr(index); }
+        const_reference operator[](size_type index) const { return *ptr(index); }
+    
         reference at(size_type index)
         {
-            if (index >= N)
-                throw std::out_of_range("Index out of range");
-            return m_data[index];
+            if (index >= N) throw std::out_of_range("gp_std::array out of range");
+            return (*this)[index];
         }
-
+    
         const_reference at(size_type index) const
         {
-            if (index >= N)
-                throw std::out_of_range("Index out of range");
-            return m_data[index];
+            if (index >= N) throw std::out_of_range("gp_std::array out of range");
+            return (*this)[index];
         }
-
-        array<T, N>& operator=(const array<T, N>& other)
-        {
-            if(this != &other)
-            {
-                for(size_t i = 0; i < N; ++i)
-                {
-                    (*this)[i] = other[i];
-                }
-            }
-            return *this;
-        }
-
-        array<T, N>& operator=(array<T, N>&& other)
-        {
-            if(this != &other)
-            {
-                for(size_t i = 0; i < N; ++i)
-                {
-                    (*this)[i] = std::move(other[i]);
-                }
-            }
-            return *this;
-        }
-
-        reference front() { return m_data[0]; }
-        const_reference front() const { return m_data[0]; }
-
-        reference back() { return m_data[N - 1]; }
-        const_reference back() const { return m_data[N - 1]; }
-
-        pointer data() { return m_data; }
-        const_pointer data() const { return m_data; }
-
-        // Iterators
-        iterator begin() { return m_data; }
-        const_iterator begin() const { return m_data; }
-        iterator end() { return m_data + N; }
-        const_iterator end() const { return m_data + N; }
-
-        // Capacity
-        constexpr size_type size() const { return N; }
-        constexpr bool empty() const { return N == 0; }
-
-        // Modifiers
-        void fill(const T &value)
+    
+        reference front() { return (*this)[0]; }
+        const_reference front() const { return (*this)[0]; }
+    
+        reference back() { return (*this)[N - 1]; }
+        const_reference back() const { return (*this)[N - 1]; }
+    
+        pointer data() { return ptr(0); }
+        const_pointer data() const { return ptr(0); }
+    
+        iterator begin() { return ptr(0); }
+        const_iterator begin() const { return ptr(0); }
+    
+        iterator end() { return ptr(0) + N; }
+        const_iterator end() const { return ptr(0) + N; }
+    
+        constexpr size_type size() const noexcept { return N; }
+        constexpr bool empty() const noexcept { return N == 0; }
+    
+        void fill(const T& value)
         {
             std::fill(begin(), end(), value);
         }
-
-        void swap(array &other) noexcept
+    
+        void swap(array& other) noexcept
         {
             std::swap_ranges(begin(), end(), other.begin());
         }
-
-        // Comparison operators (C++11 compatible)
-        bool operator==(const array &other) const
+    
+        bool operator==(const array& other) const
         {
             return std::equal(begin(), end(), other.begin());
         }
-
-        bool operator!=(const array &other) const
-        {
-            return !(*this == other);
-        }
-
-        bool operator<(const array &other) const
-        {
-            return std::lexicographical_compare(begin(), end(), other.begin(), other.end());
-        }
-
-        bool operator>(const array& other) const
-        {
-            return other < *this;
-        }
-
-        bool operator<=(const array& other) const
-        {
-            return !(other < *this);
-        }
-
-        bool operator>=(const array& other) const
-        {
-            return !(*this < other);
-        }
+    
+        bool operator!=(const array& other) const { return !(*this == other); }
+        bool operator<(const array& other) const { return std::lexicographical_compare(begin(), end(), other.begin(), other.end()); }
+        bool operator>(const array& other) const { return other < *this; }
+        bool operator<=(const array& other) const { return !(other < *this); }
+        bool operator>=(const array& other) const { return !(*this < other); }
     };
 
 } // namespace gp_std
+
 #endif
